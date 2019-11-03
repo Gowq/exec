681e6cb8ef031e500d46c7c419dc9f01e608ad80
Igor Martins <cardozo@usp.br>
2019-11-02 21:41:40 -0300

    Recursive binary search header

---
+++ rbb.h
@@ -0,0 +1,14 @@
+#ifndef _rbb
+    #define _rbb
+
+    /*
+    Recursive binary search
+        v -> search vector
+        left -> leftmost element position
+        right -> rightmost element position
+        val -> value to be searched
+    */
+    int rbb(int *v, int left, int right, int val); 
+
+
+#endif
---
+++ rbs.h
@@ -0,0 +1,14 @@
+#ifndef _rbs
+    #define _rbs
+
+    /*
+    Recursive binary search
+        v -> search vector
+        left -> leftmost element position
+        right -> rightmost element position
+        val -> value to be searched
+    */
+    int rbs(int *v, int left, int right, int val); 
+
+
+#endif
